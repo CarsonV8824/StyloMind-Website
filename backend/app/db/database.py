ea490c6046474c138c435fb2d579e874fb938a55
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2026-03-03 20:19:47 -0600

    done

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 class Database:
-    def __init__(self, db_name):
+    def __init__(self, db_name="backend/database/Stylo Mind.db"):
         self.db_name = db_name
         self.connection = sqlite3.connect(self.db_name)
         self.cursor = self.connection.cursor()
